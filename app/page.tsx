1aaab4536c4e049577e4cb2e5a6b6811405e7969
Alexis-Contente <alexis.contente@gmail.com>
2023-11-08 13:58:00 +0100

    creation du composant

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import CardList from "@/components/cardList/page";
+import Header from "@/components/header/page";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <CardList />
     </>
   );
---
+++ components/header/page.tsx
@@ -0,0 +1,9 @@
+import style from "./page.module.css";
+
+export default function Header() {
+  return (
+    <header className={style.header}>
+      <h1>Quantcube Technical Interview</h1>
+    </header>
+  );
+}
