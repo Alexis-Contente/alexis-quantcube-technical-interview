24b0253d4a5127db04e73ad08cb5dcbc43ed7a9f
Alexis-Contente <alexis.contente@gmail.com>
2023-11-08 23:22:43 +0100

    Link dans le header

--- components/header/page.tsx
+++ components/header/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import style from "./page.module.css";
 
 export default function Header() {
   return (
     <header className={style.header}>
-      <h1 className={style.title}>Quantcube Technical Interview</h1>
+      <Link className={style.title} href="/">
+        Quantcube Technical Interview
+      </Link>
     </header>
   );
 }
