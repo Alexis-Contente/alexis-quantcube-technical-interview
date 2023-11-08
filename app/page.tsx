99bba414c6b98893e52be1e81989c91368dd3fe5
Alexis-Contente <alexis.contente@gmail.com>
2023-11-08 13:45:14 +0100

    fetch api posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,9 @@
-"use client";
-
 import CardList from "@/components/cardList/page";
-import { useEffect, useState } from "react";
-
-type Posts = {
-  id: number;
-  userId: number;
-  title: string;
-  body: string;
-};
 
 export default function Home() {
-  const [posts, setPosts] = useState<Posts[]>([]);
-
-  const fetchPostsData = async () => {
-    try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      const data = await response.json();
-      console.log(data);
-      setPosts(data);
-    } catch (error) {
-      console.log("Impossible de récupérer les données de l'API", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchPostsData();
-  }, []);
-
   return (
     <>
-      {posts ? (
-        posts.map((post) => (
-          <CardList key={post.id} title={post.title} body={post.body} />
-        ))
-      ) : (
-        <p>Loading...</p>
-      )}
+      <CardList />
     </>
   );
 }
--- components/cardList/page.tsx
+++ components/cardList/page.tsx
@@ -1,19 +1,40 @@
-import style from "./page.module.css";
-type Posts = {
-  id?: number;
-  userId?: number;
-  title: string;
-  body: string;
-};
+"use client";
 
-export default function CardList(props: Posts) {
-  const { title, body } = props;
+import style from "./page.module.css";
+import { useEffect, useState } from "react";
+import { Posts } from "@/types/posts";
+
+export default function CardList() {
+  const [posts, setPosts] = useState<Posts[]>([]);
+
+  const fetchPostsData = async () => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const data = await response.json();
+      console.log(data);
+      setPosts(data);
+    } catch (error) {
+      console.log("Impossible de récupérer les données de l'API", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPostsData();
+  }, []);
   return (
     <>
-      <div className={style.card_container}>
-        <h1>{title}</h1>
-        <p>{body}</p>
-      </div>
+      {posts ? (
+        posts.map((post) => (
+          <div key={post.id} className={style.card_container}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   );
 }
