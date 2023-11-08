6924f9137b15814322b8a79658de2d2cc04b8bd8
Alexis-Contente <alexis.contente@gmail.com>
2023-11-08 22:08:26 +0100

    CSS de la home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 // IMPORTS
+import style from "./page.module.css";
 import Footer from "@/components/footer/page";
 import Header from "@/components/header/page";
 import Link from "next/link";
@@ -7,11 +8,19 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main>
-        <Link href="/users">Users</Link>
-        <Link href="/posts">Posts</Link>
-        <Link href="/albums">Albums</Link>
-        <Link href="/todos">Todos</Link>
+      <main className={style.main}>
+        <Link className={style.link} href="/users">
+          Users
+        </Link>
+        <Link className={style.link} href="/posts">
+          Posts
+        </Link>
+        <Link className={style.link} href="/albums">
+          Albums
+        </Link>
+        <Link className={style.link} href="/todos">
+          Todos
+        </Link>
       </main>
       <Footer />
     </>
