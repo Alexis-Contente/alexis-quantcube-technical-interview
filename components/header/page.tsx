f5e9e71d0e7185b61109cc106210bc834a9fc9b3
Alexis-Contente <alexis.contente@gmail.com>
2023-11-08 14:06:25 +0100

    bases du CSS

--- components/header/page.tsx
+++ components/header/page.tsx
@@ -3,7 +3,7 @@ import style from "./page.module.css";
 export default function Header() {
   return (
     <header className={style.header}>
-      <h1>Quantcube Technical Interview</h1>
+      <h1 className={style.title}>Quantcube Technical Interview</h1>
     </header>
   );
 }
