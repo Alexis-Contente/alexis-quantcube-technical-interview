3e26dbb3fc4cfd22080e5fccd07c18250daf3331
Alexis-Contente <alexis.contente@gmail.com>
2023-11-09 14:03:57 +0100

    creation du composant et du map

--- app/albums/[id]/page.tsx
+++ app/albums/[id]/page.tsx
@@ -1,11 +1,64 @@
-import Header from "@/components/header/page";
-import style from "./page.module.css";
-import Footer from "@/components/footer/page";
+"use client";
 
-export default function Album() {
+// IMPORTS
+import style from "./page.module.css";
+import Header from "@/components/header/page";
+import Footer from "@/components/footer/page";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { Photos } from "@/types/photos";
+
+export default function Album({ params }: { params: { id: string } }) {
+  // STATES
+  //Loader
+  const [loading, setLoading] = useState<boolean>(true);
+  //Album by Id
+  const [albumById, setAlbumById] = useState<Photos[]>([]);
+
+  // API CALL
+  // Get album by id
+  const fetchAlbumById = async () => {
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/photos?albumId=${params.id}`
+      );
+      const data = await response.json();
+      console.log("Album by ID: ", data);
+      setAlbumById(data);
+    } catch (error) {
+      console.log(
+        `Impossible de récupérer les datas de l'album id ${params.id}`,
+        error
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // USE EFFECT
+  useEffect(() => {
+    fetchAlbumById();
+  }, []);
+
   return (
     <>
       <Header />
+      <main className={style.main}>
+        {loading ? (
+          <p>Chargement...</p>
+        ) : (
+          albumById.map((photo) => (
+            <div key={photo.id}>
+              <Image
+                src={photo.thumbnailUrl}
+                alt={photo.title}
+                width={150}
+                height={150}
+              />
+            </div>
+          ))
+        )}
+      </main>
       <Footer />
     </>
   );
