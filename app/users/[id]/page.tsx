6e7b50360407713132e8f43eae3f812f078a897c
Alexis-Contente <alexis.contente@gmail.com>
2023-11-10 20:39:55 +0100

    call api fetch des datas postbyuserid et map des datas

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -7,6 +7,7 @@ import Footer from "@/components/footer/page";
 import { useEffect, useState } from "react";
 import { Users } from "@/types/users";
 import { Todos } from "@/types/todos";
+import { Posts } from "@/types/posts";
 import Loader from "@/components/loader/page";
 
 export default function UserById({ params }: { params: { id: string } }) {
@@ -18,7 +19,10 @@ export default function UserById({ params }: { params: { id: string } }) {
   const [userById, setUserById] = useState<Users | null>(null);
 
   // Todos by userId
-  const [todosById, setTodosById] = useState<Todos[]>([]);
+  const [todosByUserId, setTodosByUserId] = useState<Todos[]>([]);
+
+  // Posts by userId
+  const [postsByUserId, setPostsByUserId] = useState<Posts[]>([]);
 
   // CALL API
   // Get user by id
@@ -41,14 +45,14 @@ export default function UserById({ params }: { params: { id: string } }) {
   };
 
   // Get todos by user id
-  const fetchTodosById = async () => {
+  const fetchTodosByUserId = async () => {
     try {
       const response = await fetch(
         `https://jsonplaceholder.typicode.com/todos?userId=${params.id}`
       );
       const data = await response.json();
       console.log("Todos by ID: ", data);
-      setTodosById(data);
+      setTodosByUserId(data);
     } catch (error) {
       console.log(
         `Impossible de récupérer les datas des todos de l'user id ${params.id}`,
@@ -59,10 +63,30 @@ export default function UserById({ params }: { params: { id: string } }) {
     }
   };
 
+  // Get posts by user id
+  const fetchPostsByUserId = async () => {
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?userId=${params.id}`
+      );
+      const data = await response.json();
+      console.log("Posts by ID: ", data);
+      setPostsByUserId(data);
+    } catch (error) {
+      console.log(
+        `Impossible de récupérer les datas des posts de l'user id ${params.id}`,
+        error
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
   // USE EFFECT
   useEffect(() => {
     fetchUserById();
-    fetchTodosById();
+    fetchTodosByUserId();
+    fetchPostsByUserId();
   }, []);
 
   return (
@@ -99,10 +123,20 @@ export default function UserById({ params }: { params: { id: string } }) {
                 <details>
                   <summary>Todos</summary>
                   <>
-                    {todosById.map((todo) => (
+                    {todosByUserId.map((todo) => (
                       <div key={todo.id}>
                         <p>{todo.title}</p>
                         <p>{todo.completed ? "Fait" : "A faire"}</p>
+                      </div>
+                    ))}
+                  </>
+                </details>
+                <details>
+                  <summary>Posts</summary>
+                  <>
+                    {postsByUserId.map((post) => (
+                      <div key={post.id}>
+                        <p>{post.title}</p>
                       </div>
                     ))}
                   </>
